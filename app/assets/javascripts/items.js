9e6ac9f23a260c4b3fa4e759eeb21f131711b503
David Bernheisel <david@bernheisel.com>
2015-10-14 17:45:39 -0400

    Normal mode achieved

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,11 @@
+function checkPrice(){
+  setTimeout(function() {
+    $.ajax({
+      url: "/items/<%= @item.id %>",
+      type: "GET",
+      dataType: "script"
+    })}, 1500);
+    console.log("Updated price");
+};
+
+setInterval(checkPrice, 1500);
